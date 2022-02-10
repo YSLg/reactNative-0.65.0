53bc523844b6c32821c6bd8deb9f0c5527a0f863
yanghaibo <yanghaibo@baiwang.com>
2022-02-10 15:24:11 +0800

    save

--- src/components/Navigation/BottomNavigator.js
+++ src/components/Navigation/BottomNavigator.js
@@ -5,41 +5,69 @@ import {createIconSetFromFontello} from 'react-native-vector-icons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import home from '../../pages/home';
 import mine from '../../pages/mine';
-import follow from '../../pages/follow';
+import focus from '../../pages/follow';
+import * as COLOR from '../../common/Colors';
+import { ST, SS } from '../../utils';
+
+
 const IonIcons = createIconSetFromFontello(fontElloConfig);
 const Tab = createBottomTabNavigator();
 
 const HomeTabs = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          if (route.name === 'home') {
+            iconName = focused ? 'home-selected' : 'home';
+          } else if (route.name === 'focus') {
+            iconName = focused ? 'focus-selected' : 'focus';
+          } else if (route.name === 'mine') {
+            iconName = focused ? 'mine-selected' : 'mine';
+          }
+          return <IonIcons name={iconName} color={color} size={size} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#4a5377',
+        inactiveTintColor: COLOR.SColor.COLOR_808498,
+        indicatorStyle: {
+          height: 0, // tabbar 下面的一条线不显示
+        },
+        labelStyle: {
+          fontSize: ST(15),
+          position: 'relative',
+          top: SS(-6),
+        },
+        style: {
+          height: SS(76),
+        },
+        tabStyle: {
+          marginTop: 0,
+        },
+        showIcon: true,
+      }}>
       <Tab.Screen
-        name="首页"
+        name="home"
         component={home}
         options={{
           tabBarLabel: '首页',
-          tabBarIcon: ({color, size}) => (
-            <IonIcons name="home" color={color} size={size} />
-          ),
         }}
       />
       <Tab.Screen
-        name="关注"
-        component={follow}
+        name="focus"
+        component={focus}
         options={{
           tabBarLabel: '关注',
-          tabBarIcon: ({color, size}) => (
-            <IonIcons name="focus" color={color} size={size} />
-          ),
         }}
       />
       <Tab.Screen
-        name="我的"
+        name="mine"
         component={mine}
         options={{
           tabBarLabel: '我的',
-          tabBarIcon: ({color, size}) => (
-            <IonIcons name="mine" color={color} size={size} />
-          ),
         }}
       />
     </Tab.Navigator>
